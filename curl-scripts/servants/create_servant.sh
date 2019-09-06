205ff653b6e51be05bdcbd7c079ddaad4248fbd1
dtersoff <daverboy21@gmail.com>
2019-09-06 11:23:01 -0400

    Create curl-script for create feature

---
+++ curl-scripts/servants/create_servant.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl "http://localhost:4741/servants" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "servant": {
+      "name": "'"${NAME}"'",
+      "s-class": "'"${SCLASS}"'",
+      "rarity": "'"${RARITY}"'",
+      "level": "'"${LEVEL}"'",
+      "atk": "'"${ATK}"'",
+      "hp": "'"${HP}"'"
+    }
+  }'
+# :id, :name, :s-class, :rarity, :level, :atk, :hp
+echo
+
+# --header "Authorization: Token token=${TOKEN}" \
